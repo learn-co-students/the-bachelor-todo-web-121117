76b73c5d7b394b95103049154f7a7aa9718bbeab
Kenny Hom <kenny.hom27@gmail.com>
2017-11-21 02:49:56 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,21 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.map { |year, girls| girls.each { |girl| return girl["name"].split(" ").first if year == season && girl["status"] == "Winner"} }
 end
 
-def get_contestant_name(data, occupation)
-  # code here
+def get_contestant_name(data, occ)
+  data.map { |year, girls| girls.each { |girl| return girl["name"] if girl["occupation"] == occ }}
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  data.map { |year, girls| girls.map { |girl| girl["hometown"] == hometown ? girl["name"] : nil }.compact }.flatten.count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.map { |year, girls| girls.map { |girl| return girl["occupation"] if girl["hometown"] == hometown } }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total = []
+  data.map { |year, girls| girls.select { |girl| total << girl["age"].to_i if year == season}}
+  (total.reduce(:+) / total.count.to_f).round
 end
