b80c947515a9c9842ee99e26540d16d60d0c7da5
josephdlawson21 <josephdlawson21@gmail.com>
2017-11-21 13:35:57 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  result = []
+  data.each do |season_num,contestant_data|
+    if season_num == season
+      # binding.pry
+      contestant_data.each do |catagory|
+        if catagory["status"] == "Winner"
+          result = catagory["name"]
+        end
+      end
+    end
+  end
+  result.split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  result = []
+  data.each do |season_num,contestant_data|
+    contestant_data.each do |catagory|
+      if catagory["occupation"] == occupation
+        result = catagory["name"]
+      end
+    end
+  end
+  result
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  result = 0
+  data.each do |season_num,contestant_data|
+    contestant_data.each do |catagory|
+      if catagory["hometown"] == hometown
+        result += 1
+      end
+    end
+  end
+  result
 end
 
 def get_occupation(data, hometown)
-  # code here
+  result = 0
+  data.each do |season_num,contestant_data|
+    contestant_data.each do |catagory|
+      if catagory["hometown"] == hometown
+        return catagory["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  result = []
+  data.each do |season_num,contestant_data|
+    if season_num == season
+      contestant_data.each do |catagory|
+        result << catagory["age"].to_i
+      end
+    end
+  end
+  result = result.inject{ |sum, el| sum + el }.to_f / result.size
+  result.round
 end
