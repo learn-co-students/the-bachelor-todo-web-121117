d24acf8cc85f8fc1428c4fe8905266b1fe8fcb95
Matthew Welch <dmw9400@gmail.com>
2017-11-25 21:12:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -69,9 +69,21 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  # Build a method `get_occupation`, that takes in two arguments––the data hash and a string of a hometown.
+  # It returns the occupation of the first contestant who hails from that hometown.
+  hometown_array = []
+     data.each do |season, information|
+       information.each do |stats|
+         if stats["hometown"] == hometown
+           hometown_array << stats["occupation"]
+         end
+       end
+     end
+     return hometown_array[0]
+
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  
 end
